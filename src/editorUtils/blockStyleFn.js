ba69c3f79e743465c05d7b7d97961491e444e99a
lcc19941214 <245987239@qq.com>
2017-09-24 14:38:12 +0800

    style: initial atomic

--- src/editorUtils/blockStyleFn.js
+++ src/editorUtils/blockStyleFn.js
@@ -1,15 +1,25 @@
 import { DEFAULT_COLOR, DEFAULT_FONT_SIZE } from './inlineStyles';
 
-export default contentBlock => {
-  const type = contentBlock.getType();
-  switch (type) {
-    case 'unstyled':
-      return 'RichEditor-style__initial-unstyled';
-    default:
-  }
+export const FIGURE = {
+  margin: '1em 0'
+};
+
+export const INITIAL_ATOMIC = {
+  ...FIGURE
 };
 
 export const INITIAL_UNSTYLED = {
   ...DEFAULT_COLOR,
   ...DEFAULT_FONT_SIZE
+};
+
+export default contentBlock => {
+  const type = contentBlock.getType();
+  switch (type) {
+    case 'unstyled':
+      return 'RichEditor-style__initial-unstyled';
+    case 'atomic':
+      return 'RichEditor-style__initial-atomic';
+    default:
+  }
 };
